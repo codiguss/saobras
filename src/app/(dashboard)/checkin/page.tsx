b4598d541bf72fdef45147326a1364d52845cec1
Zorgbr <Eliasmel300@gmail.com>
2026-09-15 22:28:15 -0300

    configurando pagina presenca 3
    Simplified CheckinPage component by removing unnecessary div wrapper.

--- src/app/(dashboard)/checkin/page.tsx
+++ src/app/(dashboard)/checkin/page.tsx
@@ -1,9 +1,5 @@
 import FormCheckin from "@/components/checkin/FormCheckin";
 
 export default function CheckinPage() {
-  return (
-    <div className="p-6 md:p-8">
-      <FormCheckin />
-    </div>
-  );
+  return <FormCheckin />;
 }
